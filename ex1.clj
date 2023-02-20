21f71adc62ff42fb7675df10e2f6add2b5ab4f0e
Helder <helder.deoliveira@codurance.com>
2023-02-20 20:34:20 +0100

    fizzbuzz

--- ex1.clj
+++ ex1.clj
@@ -25,6 +25,7 @@
 (println (class :hello))
 (println (class 'x))
 
+(println "----------------------- OPERATIONS -----------------------")
 (println (+ 1 1))
 (println (+ 1 1 1 1 1 1 ))
 (println (* 2 4))
@@ -35,10 +36,13 @@
 (println '(+ 1 2))
 (println (eval '(+ 1 2)))
 
+(println "----------------------- CALLING JAVA -----------------------")
 (.println System/out "hello world")
 
+(println "----------------------- EXCEPTIONS -----------------------")
 (println (try (throw (Exception. "hello exception!")) (catch Exception e (.getMessage e))))
 
+(println "----------------------- LIST -----------------------")
 (def x '(1 2 3 4 5))
 (println (class x))
 (println (first x))
@@ -48,6 +52,7 @@
 (println (pop x))
 (println (peek x))
 
+(println "----------------------- VECTOR -----------------------")
 (def y [1 2 3 4 6])
 (println (class y))
 (println (nth y 2))
@@ -56,9 +61,11 @@
 (println (conj x 6))
 (println (conj y 6))
 
+
 (def t1 [1 "hello"])
 (apply println t1)
 
+(println "----------------------- CONDITIONS -----------------------")
 (println (if (= 1 1) "hello" "world"))
 (println (if (= 1 2) "hello" "world"))
 
@@ -70,7 +77,19 @@
 
 (println (cond (= x 3) "hello" (= y 5) "world")) ; evaluate the expr, if true, then returns
 
+(println "----------------------- FUNCTIONS -----------------------")
 (defn say-name [n] (println "Hi " n))
 (say-name "Helder")
 
 (println (macroexpand '(defn say-hello [n] n)))
+
+(println "----------------------- FIZZBUZZ -----------------------")
+(defn fizzbuzz [ns]
+  (for [n ns]
+    (cond
+      (= 0 (mod n 15)) "FizzBuzz"
+      (= 0 (mod n 3)) "Fizz"
+      (= 0 (mod n 5)) "Buzz"
+      :else n
+    )))
+(println (fizzbuzz '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)))
